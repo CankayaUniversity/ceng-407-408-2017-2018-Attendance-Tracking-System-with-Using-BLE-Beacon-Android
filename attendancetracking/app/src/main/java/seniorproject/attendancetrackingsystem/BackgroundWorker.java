12e9d2565c8c071034c913b06ec129980bb9c306
Melihşah AKIN <asalyazilim@gmail.com>
2018-03-11 14:24:30 +0300

    Bug fixes

--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/BackgroundWorker.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/BackgroundWorker.java
@@ -17,9 +17,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
-/**
- * Created by Melihşah AKIN on 10.03.2018.
- */
 
 public class BackgroundWorker extends AsyncTask<String,Void,String>{
     Context context;
@@ -41,9 +38,15 @@ public class BackgroundWorker extends AsyncTask<String,Void,String>{
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-                String post_data = URLEncoder.encode("username","UTF-8" )+"="+URLEncoder.encode(username,"UTF-8") +"&"
-                        +URLEncoder.encode("password","UTF-8" )+"="+URLEncoder.encode(password,"UTF-8");
+
+
+                BufferedWriter bufferedWriter = new BufferedWriter(
+                        new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data = URLEncoder.encode("username","UTF-8" )
+                        + "="+URLEncoder.encode(username,"UTF-8") +"&"
+                        + URLEncoder.encode("password","UTF-8" )+"="
+                        + URLEncoder.encode(password,"UTF-8");
+
 
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
@@ -51,11 +54,16 @@ public class BackgroundWorker extends AsyncTask<String,Void,String>{
                 outputStream.close();
 
                 InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
-                String result="";
-                String line="";
+
+
+
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
                 while((line = bufferedReader.readLine()) != null){
-                    result+=line;
+                    result += line;
+
                 }
 
                 bufferedReader.close();
