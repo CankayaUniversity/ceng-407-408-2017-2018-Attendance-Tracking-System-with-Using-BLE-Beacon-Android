ea31936200ce7d4aca3fb5d8a97facda329fa5da
Mertcan Kokcur <mertcan.kokcur@outlook.com>
2018-03-11 13:37:49 +0300

    Registration Interface and Some Attribute Identification

--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/LoginStudentActivity.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/LoginStudentActivity.java
@@ -3,11 +3,11 @@ package seniorproject.attendancetrackingsystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginStudentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_student_login);
     }
 }
