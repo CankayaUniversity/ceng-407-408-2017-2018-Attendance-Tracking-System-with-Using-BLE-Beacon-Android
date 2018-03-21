5763dc56a7b5dfaef5b7fb9838257cd41c19d333
Melihşah AKIN <asalyazilim@gmail.com>
2018-03-21 21:06:15 +0300

    SessionManager class is added.
    This class provide session management like Login and logout operations.

---
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/SessionManager.java
@@ -0,0 +1,63 @@
+package seniorproject.attendancetrackingsystem;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import java.util.HashMap;
+
+public class SessionManager {
+    private SharedPreferences pref;
+    private Editor editor;
+    Context context;
+    private static final String PREF_NAME = "SessionPref";
+    private static final String IS_LOGIN = "IsLoggedIn";
+    private static final String KEY_USERTYPE = "userType";
+    private static final String KEY_USERNAME = "username";
+    private static final int PRIVATE_MODE = 0;
+
+    public SessionManager(Context context) {
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void createLoginSession(String userType, String username) {
+        editor.putBoolean(IS_LOGIN, true);
+        editor.putString(KEY_USERNAME, username);
+        editor.putString(KEY_USERTYPE, userType);
+
+        editor.commit();
+    }
+
+    public void checkLogin() {
+        if (!this.isLoggedIn()) {
+            Intent intent = new Intent(context, MainLoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        }
+    }
+
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        user.put(KEY_USERTYPE, pref.getString(KEY_USERTYPE, null));
+        user.put(KEY_USERNAME, pref.getString(KEY_USERNAME, null));
+        return user;
+    }
+
+    public void logoutUser() {
+        editor.clear();
+        editor.commit();
+        Intent intent = new Intent(context, MainLoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    public boolean isLoggedIn() {
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
