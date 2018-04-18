d937dc632eeced265488d231d0bd4ac2da4703e5
Melihşah AKIN <asalyazilim@gmail.com>
2018-04-18 13:44:18 +0300

    Beacon Builder is changed beacause of changes on beacon SDK

--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/activities/LecturerActivity.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/activities/LecturerActivity.java
@@ -1,8 +1,19 @@
 package seniorproject.attendancetrackingsystem.activities;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +21,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -19,18 +30,31 @@ import seniorproject.attendancetrackingsystem.R;
 import seniorproject.attendancetrackingsystem.fragments.ReportFragmentLecturer;
 import seniorproject.attendancetrackingsystem.fragments.WelcomeFragmentLecturer;
 import seniorproject.attendancetrackingsystem.helpers.BeaconBuilder;
+import seniorproject.attendancetrackingsystem.helpers.DatabaseManager;
 import seniorproject.attendancetrackingsystem.helpers.SessionManager;
-import seniorproject.attendancetrackingsystem.utils.Actor;
-import seniorproject.attendancetrackingsystem.utils.Globals;
-import seniorproject.attendancetrackingsystem.utils.Lecturer;
-import seniorproject.attendancetrackingsystem.utils.Student;
 
 public class LecturerActivity extends AppCompatActivity {
-
+  private BeaconBuilder beaconBuilder;
+  private boolean mServiceBound = false;
   private BottomNavigationView mainNav;
-
+  private AlertDialog alertDialog;
+  private ProgressDialog progressDialog;
   private WelcomeFragmentLecturer welcomeFragmentLecturer;
   private ReportFragmentLecturer reportFragmentLecturer;
+  private ServiceConnection serviceConnection =
+      new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+          BeaconBuilder.ServiceBinder binder = (BeaconBuilder.ServiceBinder) service;
+          beaconBuilder = binder.getService();
+          mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+          mServiceBound = false;
+        }
+      };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +101,14 @@ public class LecturerActivity extends AppCompatActivity {
             return true;
           }
         });
+    Receiver mReceiver = new Receiver();
+    IntentFilter filter = new IntentFilter();
+    filter.addAction(BeaconBuilder.ACTION);
+    registerReceiver(mReceiver, filter);
+    alertDialog = new AlertDialog.Builder(this).create();
+    alertDialog.setCanceledOnTouchOutside(false);
+    progressDialog = new ProgressDialog(this);
+    progressDialog.setCanceledOnTouchOutside(false);
   }
 
   private void setFragment(Fragment fragment) {
@@ -95,8 +127,12 @@ public class LecturerActivity extends AppCompatActivity {
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     setFinishOnTouchOutside(false);
-    if(item.toString().equals("Beacon Configuration")){
-      new BeaconBuilder(this);
+    if (item.toString().equals("Beacon Configuration")) {
+      showProgressDialog();
+      Intent intent = new Intent(this, BeaconBuilder.class);
+     // startService(intent);
+      bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+      // new BeaconBuilder();
     }
     return super.onOptionsItemSelected(item);
   }
@@ -106,5 +142,74 @@ public class LecturerActivity extends AppCompatActivity {
     setFragment(welcomeFragmentLecturer);
     mainNav.setSelectedItemId(R.id.nav_home);
   }
+private void showProgressDialog(){
+    progressDialog.setTitle("Beacon syncronizer");
+    progressDialog.setMessage("Searching nearyby beacons");
+    progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        BluetoothAdapter.getDefaultAdapter().disable();
+        if(mServiceBound){
+          unbindService(serviceConnection);
+          mServiceBound = false;
+        }
+      }
+    });
+    progressDialog.show();
+}
+  private void showAlertDialog(final String mac) {
+    progressDialog.hide();
+    alertDialog.setTitle("Found Beacon");
+    alertDialog.setMessage("MAC: " + mac);
+    alertDialog.setButton(
+        DialogInterface.BUTTON_NEGATIVE,
+        "Ignore",
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            beaconBuilder.addToIgnoreList(mac);
+            beaconBuilder.continueTracking();
+            showProgressDialog();
+          }
+        });
+    alertDialog.setButton(
+        DialogInterface.BUTTON_POSITIVE,
+        "Save",
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            Map<String, String> postParameters = new HashMap<>();
+            postParameters.put("beacon_mac", mac);
+            postParameters.put(
+                "user_id",
+                new SessionManager(getApplicationContext())
+                    .getUserDetails()
+                    .get(SessionManager.KEY_USER_ID));
+            DatabaseManager.getmInstance(getApplicationContext())
+                .execute("set-beacon", postParameters);
+            BluetoothAdapter.getDefaultAdapter().disable();
+            if(mServiceBound){
+              unbindService(serviceConnection);
+              mServiceBound = false;
+            }
+          //  stopService(new Intent(LecturerActivity.this, BeaconBuilder.class));
+          }
+        });
 
+    alertDialog.show();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    unbindService(serviceConnection);
+    mServiceBound = false;
+  }
+
+  private class Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      showAlertDialog(intent.getStringExtra("MAC"));
+    }
+  }
 }
--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/helpers/BeaconBuilder.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/helpers/BeaconBuilder.java
@@ -1,139 +1,120 @@
 package seniorproject.attendancetrackingsystem.helpers;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
-import com.aprilbrother.aprilbrothersdk.Beacon;
-import com.aprilbrother.aprilbrothersdk.BeaconManager;
-import com.aprilbrother.aprilbrothersdk.Region;
-import com.aprilbrother.aprilbrothersdk.utils.AprilL;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.Comparator;
 
-public class BeaconBuilder {
-  private static final Region ALL_BEACONS_REGION = new Region("REGION01", null, null, null);
-  private ProgressDialog progressDialog;
-  private AlertDialog alertDialog;
-  private Context context;
+public class BeaconBuilder extends Service implements BeaconConsumer {
+  public final static String ACTION = "BeaconBuilder";
+  private static final Region ALL_BEACONS= new Region("ALL_BEACONS", null, null, null);
   private BeaconManager beaconManager;
   private ArrayList<Beacon> beacons;
-  private LinkedList<String> ignoreList;
+  private ArrayList<String> ignoreList;
+  private IBinder mBinder = new ServiceBinder();
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    if(!BluetoothAdapter.getDefaultAdapter().isEnabled())
+      BluetoothAdapter.getDefaultAdapter().enable();
+    beacons = new ArrayList<>();
+    ignoreList = new ArrayList<>();
+    beaconManager = BeaconManager.getInstanceForApplication(this);
+    beaconManager
+        .getBeaconParsers()
+        .add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-  public BeaconBuilder(Context context) {
-    this.context = context;
-    progressDialog = new ProgressDialog(context);
-    progressDialog.setTitle("Beacon syncronizer");
-    progressDialog.setMessage("GOING");
-    alertDialog = new AlertDialog.Builder(context).create();
-    alertDialog.setTitle("Beacon is detected");
-    progressDialog.setButton(
-        DialogInterface.BUTTON_NEGATIVE,
-        "Cancel",
-        new DialogInterface.OnClickListener() {
+    beaconManager.bind(this);
+  }
+
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return mBinder;
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    beaconManager.unbind(this);
+  }
+
+  @Override
+  public void onBeaconServiceConnect() {
+    try {
+      beaconManager.startRangingBeaconsInRegion(ALL_BEACONS);
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    }
+
+    beaconManager.addRangeNotifier(
+        new RangeNotifier() {
           @Override
-          public void onClick(DialogInterface dialog, int which) {
-            try {
-              beaconManager.stopRanging(ALL_BEACONS_REGION);
-            } catch (RemoteException e) {
-              e.printStackTrace();
+          public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+            beacons.clear();
+            beacons.addAll(collection);
+            Collections.sort(beacons, new Comparator<Beacon>() {
+              @Override
+              public int compare(Beacon o1, Beacon o2) {
+                return Integer.compare(o1.getRssi(), o2.getRssi());
+              }
+            });
+            if(beacons.size() > 0 && getFirstBeacon() != -1){
+              Log.i("hello", beacons.get(getFirstBeacon()).getBluetoothAddress());
+              try{
+                beaconManager.stopRangingBeaconsInRegion(ALL_BEACONS);
+              }catch (RemoteException e){
+                e.printStackTrace();
+              }
+              Intent  intent = new Intent();
+              intent.setAction(ACTION);
+              intent.putExtra("MAC", beacons.get(getFirstBeacon()).getBluetoothAddress());
+              sendBroadcast(intent);
             }
           }
         });
-    initialize();
-    connectToTheService();
   }
 
   private int getFirstBeacon() {
     int index = 0;
     for (Beacon x : beacons) {
-      if (!ignoreList.contains(x.getMacAddress())) return index;
+      if (!ignoreList.contains(x.getBluetoothAddress())) return index;
       index++;
     }
     return -1;
   }
 
-  private void initialize() {
-    BluetoothAdapter.getDefaultAdapter().enable();
-    beacons = new ArrayList<>();
-    ignoreList = new LinkedList<>();
-    AprilL.enableDebugLogging(true);
-    beaconManager = new BeaconManager(context.getApplicationContext());
-    beaconManager.setRangingListener(
-        new BeaconManager.RangingListener() {
-          @Override
-          public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-            beacons.clear();
-            beacons.addAll(list);
-
-            ComparatorBeaconByRssi com = new ComparatorBeaconByRssi();
-            Collections.sort(beacons, com);
-
-            if (beacons.size() > 0 && getFirstBeacon() != -1) {
-              try {
-                final String mac = beacons.get(getFirstBeacon()).getMacAddress();
-                progressDialog.setMessage("Beacon: " + beacons.get(0).getMacAddress());
-                // TODO CONFIRMATION DIALOG WILL BE CALLED
-                beaconManager.stopRanging(ALL_BEACONS_REGION);
-                progressDialog.hide();
-                alertDialog.setMessage("Mac address: " + mac);
-                alertDialog.setButton(
-                    DialogInterface.BUTTON_NEGATIVE,
-                    "Ignore",
-                    new DialogInterface.OnClickListener() {
-                      @Override
-                      public void onClick(DialogInterface dialog, int which) {
-                        ignoreList.add(mac);
-                        connectToTheService();
-                      }
-                    });
-                alertDialog.setButton(
-                    DialogInterface.BUTTON_POSITIVE,
-                    "Save",
-                    new DialogInterface.OnClickListener() {
-                      @Override
-                      public void onClick(DialogInterface dialog, int which) {
-                        Map<String, String> postParameters = new HashMap<>();
-                        postParameters.put("beacon_mac", mac);
-                        SessionManager session = new SessionManager(context);
-                        Map<String, String> userInfo = session.getUserDetails();
-                        postParameters.put("user_id", userInfo.get(SessionManager.KEY_USER_ID));
-                        DatabaseManager.getmInstance(context).execute("set-beacon", postParameters);
-                        BluetoothAdapter.getDefaultAdapter().disable();
-                      }
-                    });
-                alertDialog.show();
-
-              } catch (RemoteException e) {
-                e.printStackTrace();
-              }
-            }
-          }
-        });
+  public void addToIgnoreList(String mac){
+    ignoreList.add(mac);
+    Log.i("IGNORE", mac);
   }
 
-  private void connectToTheService() {
-    progressDialog.setMessage("Searching...");
-    progressDialog.show();
-    alertDialog.hide();
-    beaconManager.connect(
-        new BeaconManager.ServiceReadyCallback() {
-          @Override
-          public void onServiceReady() {
-            try {
-              beaconManager.startRanging(ALL_BEACONS_REGION);
-            } catch (RemoteException e) {
-              e.printStackTrace();
-            }
-          }
-        });
+  public void continueTracking(){
+    try{
+      beaconManager.startRangingBeaconsInRegion(ALL_BEACONS);
+    }catch (RemoteException e){
+      e.printStackTrace();
+    }
   }
+public class ServiceBinder extends Binder {
+    public BeaconBuilder getService(){
+      return  BeaconBuilder.this;
+    }
+}
 }
