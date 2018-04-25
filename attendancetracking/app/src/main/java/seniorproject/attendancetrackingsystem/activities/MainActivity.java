2740e71a754bcd36e0a92ce623cc5d06dc28ca3a
Melihşah AKIN <asalyazilim@gmail.com>
2018-04-26 00:11:31 +0300

    Permission checker on start if the SDK level  > 23

--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/activities/MainActivity.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/activities/MainActivity.java
@@ -1,11 +1,15 @@
 package seniorproject.attendancetrackingsystem.activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
 
 import java.util.Map;
 import java.util.Objects;
@@ -14,25 +18,15 @@ import seniorproject.attendancetrackingsystem.R;
 import seniorproject.attendancetrackingsystem.helpers.SessionManager;
 
 public class MainActivity extends AppCompatActivity {
+  private Button login;
+  private Button register;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    SessionManager session = new SessionManager(getApplicationContext());
-    if (session.isLoggedIn()) {
-      Map<String, String> userInfo = session.getUserDetails();
-      Intent intent;
-      if(userInfo.get(SessionManager.KEY_USER_TYPE).equals("student"))
-        intent = new Intent(MainActivity.this, StudentActivity.class);
-        else
-          intent = new Intent(MainActivity.this, LecturerActivity.class);
-      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      startActivity(intent);
-    }
-    Button login = findViewById(R.id.login_button);
-    Button register = findViewById(R.id.register_button);
+    login = findViewById(R.id.login_button);
+    register = findViewById(R.id.register_button);
     Intent intent = getIntent();
     Bundle bundle = intent.getExtras();
 
@@ -44,27 +38,66 @@ public class MainActivity extends AppCompatActivity {
         e.printStackTrace();
       }
     }
-    login.setOnClickListener(
-            new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
-              }
-            });
+    checkPermissions();
+  }
 
-    register.setOnClickListener(
-            new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                Intent registerIntent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(registerIntent);
-              }
-            });
+  private void checkPermissions() {
+    Permissions.check(
+        this,
+        new String[] {
+          Manifest.permission.CAMERA,
+          Manifest.permission.ACCESS_COARSE_LOCATION,
+          Manifest.permission.INTERNET,
+          Manifest.permission.BLUETOOTH,
+          Manifest.permission.BLUETOOTH_ADMIN,
+          Manifest.permission.ACCESS_FINE_LOCATION,
+          Manifest.permission.READ_PHONE_STATE,
+          Manifest.permission.ACCESS_NETWORK_STATE
+        },
+        "These permissions are required to use the system properly",
+        new Permissions.Options()
+            .setSettingsDialogTitle("Permission Requirement!")
+            .setRationaleDialogTitle("Info"),
+        new PermissionHandler() {
+          @Override
+          public void onGranted() {
+            checkSession();
+            login.setOnClickListener(
+                new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                  }
+                });
+
+            register.setOnClickListener(
+                new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                    Intent registerIntent =
+                        new Intent(MainActivity.this, RegistrationActivity.class);
+                    startActivity(registerIntent);
+                  }
+                });
+          }
+        });
+  }
+
+  private void checkSession() {
+    SessionManager session = new SessionManager(getApplicationContext());
+    if (session.isLoggedIn()) {
+      Map<String, String> userInfo = session.getUserDetails();
+      Intent intent;
+      if (userInfo.get(SessionManager.KEY_USER_TYPE).equals("student"))
+        intent = new Intent(MainActivity.this, StudentActivity.class);
+      else intent = new Intent(MainActivity.this, LecturerActivity.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      startActivity(intent);
+    }
   }
 
   @Override
-  public void onBackPressed() {
-
-  }
+  public void onBackPressed() {}
 }
