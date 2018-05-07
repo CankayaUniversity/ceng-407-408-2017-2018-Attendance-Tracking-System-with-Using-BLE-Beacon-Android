1f9aeb1ea4296e067333923531f748c9898f6249
Berkan Gürel <berkan_gurel1903@hotmail.com>
2018-05-08 01:04:32 +0300

    Student list in the current classroom

--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/fragments/ReportFragment.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/fragments/ReportFragment.java
@@ -2,24 +2,117 @@ package seniorproject.attendancetrackingsystem.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import seniorproject.attendancetrackingsystem.R;
+import seniorproject.attendancetrackingsystem.helpers.DatabaseManager;
+import seniorproject.attendancetrackingsystem.helpers.JsonHelper;
+import seniorproject.attendancetrackingsystem.helpers.SessionManager;
+import seniorproject.attendancetrackingsystem.utils.Actor;
+import seniorproject.attendancetrackingsystem.utils.Course;
+import seniorproject.attendancetrackingsystem.utils.Globals;
+import seniorproject.attendancetrackingsystem.utils.Student;
+import seniorproject.attendancetrackingsystem.utils.TakenCourses;
 
 /* A simple {@link Fragment} subclass. */
 public class ReportFragment extends Fragment {
 
+  private Spinner studentlecturelist;
+  private ArrayList<String> studentlecture;
+
   public ReportFragment() {
     // Required empty public constructor
+
   }
+
+
 
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_report, container, false);
   }
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    initElements(view);
+  }
+
+  private void initElements(View view) {
+    studentlecturelist =view.findViewById(R.id.studentlecturelist);
+    Actor loggedUser = ((Globals)getActivity().getApplicationContext()).getLoggedUser();
+    ArrayList<TakenCourses> list = null;
+    if( ((Student)loggedUser).getTakenCourses()!=null) list =  ((Student)loggedUser).getTakenCourses();
+    if(list == null){
+      Map<String,String> userInfo = new SessionManager(getActivity().getApplicationContext()).getUserDetails();
+      Map<String,String> param = new HashMap<>();
+      param.put("user_id", userInfo.get(SessionManager.KEY_USER_ID));
+
+      DatabaseManager.getmInstance(getActivity().getApplicationContext()).execute("get-taken-courses", param);
+      list = ((Student)loggedUser).getTakenCourses();
+    }
+
+    ArrayList<String> courseCodes = getCourseList(list);
+    for(String x : courseCodes) Log.d("COURSE CIDE", x);
+  }
+
+  private ArrayList<String> getCourseList( final ArrayList<TakenCourses> list){
+    final ArrayList<String>  takenCourseStrings = new ArrayList<>();
+    if(((Globals) getActivity().getApplicationContext()).getCourses() != null){
+      for(Course x : ((Globals)getActivity().getApplicationContext()).getCourses()){
+        for(TakenCourses y : list){
+          if(x.getCourseId() == y.getCourse_id()) takenCourseStrings.add(x.getCourseCode());
+        }
+      }
+    return takenCourseStrings;
+    }
+    StringRequest request = new StringRequest(Request.Method.POST, DatabaseManager.GetOperations, new Response.Listener<String>() {
+      @Override
+      public void onResponse(String response) {
+        JsonHelper jsonHelper = JsonHelper.getmInstance(getActivity().getApplicationContext());
+        ((Globals) getActivity().getApplicationContext())
+                .setCourses(jsonHelper.parseCourseList(response));
+        for (Course course :
+                ((Globals) getActivity().getApplicationContext()).getCourses()) {
+          for(TakenCourses y : list){
+            if(course.getCourseId() == y.getCourse_id()) takenCourseStrings.add(course.getCourseCode());
+          }
+        }
+
+      }
+    }, new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+
+      }
+    }){
+      @Override
+      protected Map<String, String> getParams(){
+        Map<String, String> postParameters = new HashMap<>();
+        postParameters.put("operation", "course-list");
+        return postParameters;
+      }
+    };
+    DatabaseManager.getmInstance(getActivity().getApplicationContext()).execute(request);
+    return takenCourseStrings;
+  }
+
 }
--- attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/fragments/ReportFragmentLecturer.java
+++ attendancetracking/app/src/main/java/seniorproject/attendancetrackingsystem/fragments/ReportFragmentLecturer.java
@@ -1,10 +1,21 @@
 package seniorproject.attendancetrackingsystem.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
 
 import seniorproject.attendancetrackingsystem.R;
 
@@ -14,6 +25,8 @@ public class ReportFragmentLecturer extends Fragment {
   public ReportFragmentLecturer() {
     // Required empty public constructor
   }
+  ListView listView;
+
 
   @Override
   public View onCreateView(
@@ -21,5 +34,89 @@ public class ReportFragmentLecturer extends Fragment {
 
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_report_lecturer, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    listView = view.findViewById(R.id.studentlist);
+
+    //TODO get from database with the current classroom
+    StudentRow student_data[] = new StudentRow[]{
+            new StudentRow("Melihşah Akın",201411202, 2),
+            new StudentRow("Ensar Elmas",201411545, 2),
+            new StudentRow("N. Cem Altunbulduk",201411341, 2),
+            new StudentRow("Merve Şanlı",201411231, 1),
+            new StudentRow("D. Mertcan Kökcür", 201411036, 1),
+            new StudentRow("Bahar Şengez", 201511374, 0),
+            new StudentRow("Berkan Gürel", 201611672,0),
+            new StudentRow("Buğra Gülay",201411909, 0),
+            new StudentRow("Koray Çıbık", 201611677, 0)
+    };
+
+
+    StudentAdapter adapter = new StudentAdapter(getActivity(), R.layout.liststudent,student_data);
+    listView.setAdapter(adapter);
+
+  }
+
+  public class StudentRow {
+    public String name;
+    public int number;
+    public int state;
+
+    public StudentRow(String name, int number, int state){
+      this.name = name;
+      this.number = number;
+      this.state = state;
+    }
+    public StudentRow(){super();}
+  }
+  public class StudentAdapter extends ArrayAdapter<StudentRow>{
+    Context context;
+    int layoutResourseId;
+    StudentRow data[] = null;
+
+    public StudentAdapter(Context context, int layoutResourseId, StudentRow[] data){
+      super(context, layoutResourseId, data);
+      this.layoutResourseId = layoutResourseId;
+      this.context = context;
+      this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+      View row = convertView;
+      StudentHolder holder = null;
+
+      if(row == null){
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        row = inflater.inflate(layoutResourseId, parent, false);
+
+        holder = new StudentHolder();
+        holder.txtName = (TextView)row.findViewById(R.id.studentName);
+        holder.txtNumber = (TextView)row.findViewById(R.id.studentNo);
+
+        row.setTag(holder);
+      }else
+      {
+        holder = (StudentHolder)row.getTag();
+      }
+
+      StudentRow student = data[position];
+      holder.txtName.setText(student.name);
+      holder.txtNumber.setText(String.valueOf(student.number));
+      if(student.state == 0) row.setBackgroundColor(getResources().getColor(R.color.stateRed));
+      else if(student.state == 1) row.setBackgroundColor(getResources().getColor(R.color.stateYellow));
+      else if(student.state == 2) row.setBackgroundColor(getResources().getColor(R.color.stateGreen));
+
+      return row;
+    }
+   class StudentHolder{
+      TextView txtNumber;
+      TextView txtName;
+  }
   }
 }
